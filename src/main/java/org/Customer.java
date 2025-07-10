42c23e73254cac76f8d0e71aec8be88ce59c79b2
seriy_p <seryi_pv@mail.ru>
2025-07-10 09:57:56 +0300

    Add Customer data

---
+++ src/main/java/org/Customer.java
@@ -0,0 +1,20 @@
+package org;
+
+// Класс сообщения для kafka
+public class Customer {
+    private int customerID;
+    private String customerName;
+
+    public Customer(int customerID, String customerName) {
+        this.customerID = customerID;
+        this.customerName = customerName;
+    }
+
+    public int getID() {
+        return customerID;
+    }
+
+    public String getName() {
+        return customerName;
+    }
+}
